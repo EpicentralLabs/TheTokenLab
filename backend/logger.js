572bafecc44ff2ce002c470d1ba562f324a7a302
Mitch <mitch.clements@gbtel.ca>
2024-09-22 11:40:33 -0400

    Moved logging init to separate file

---
+++ backend/logger.js
@@ -0,0 +1,17 @@
+// logger.js
+import winston from 'winston';
+
+const logger = winston.createLogger({
+    level: 'debug',
+    format: winston.format.combine(
+        winston.format.timestamp(),
+        winston.format.printf(({ timestamp, level, message }) => {
+            return `${timestamp} [${level}]: ${message}`;
+        })
+    ),
+    transports: [
+        new winston.transports.Console(),
+    ],
+});
+
+export default logger;
