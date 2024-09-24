13cf0c3f98a15ef86d7fb57f746415ddc803a107
TheLazySol <tbelonog25@gmail.com>
2024-09-23 21:31:17 -0400

    InitializeMint-message components created

---
+++ src/components/InitalizingMint-message.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './InitalizingMint-message.css'
+
+function InitalizingMintMessage() {
+  return (
+    <div className="initializing-mint-message fade-in">
+      Initializing mint... Please confirm any signatures when your wallet pops up!
+    </div>
+  )
+}
+
+export default InitalizingMintMessage
