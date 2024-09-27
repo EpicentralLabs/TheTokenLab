293c788815085ba4eba8adf200faf1b64164e8d6
Zero <68938738+ZeroSums@users.noreply.github.com>
2024-09-26 22:54:50 -0400

    mintTokens.mjs changes

--- backend/mintTokens.mjs
+++ backend/mintTokens.mjs
@@ -1,32 +1,33 @@
 import logger from "./logger.mjs";
 import { getOrCreateAssociatedTokenAccount, mintTo } from "@solana/spl-token";
-import {PublicKey} from "@solana/web3.js";
 
 export async function mintTokens(connection, mint, quantity, payer, decimals, paymentType) {
-    // Ensure mint is a PublicKey instance
-    if (!(mint instanceof PublicKey)) {
-        throw new Error('Mint must be a PublicKey instance.');
+    try {
+        logger.info('Payer:', payer.publicKey.toBase58());
+        logger.info('Mint:', mint.toBase58());
+        logger.info('Quantity:', quantity);
+        logger.info('Payment Type:', paymentType);
+
+        if (!payer || !payer.publicKey) {
+            throw new Error('Payer is not defined or missing publicKey');
+        }
+
+        const payerTokenAccount = await getOrCreateAssociatedTokenAccount(connection, payer, mint, payer.publicKey, decimals);
+        logger.info('Payer Token Account:', payerTokenAccount.address.toBase58());
+
+        await mintTo(
+            connection,
+            payer,
+            mint,
+            payerTokenAccount.address,
+            payer.publicKey,
+            quantity * Math.pow(10, decimals)
+        );
+
+        logger.info(`Minted ${quantity} tokens to payer token account ${payerTokenAccount.address.toBase58()} under payment type ${paymentType}`);
+        return payerTokenAccount;
+    } catch (error) {
+        logger.error(`Error in mintTokens: ${error.message}`);
+        throw error;
     }
-
-    console.log('Payer PublicKey:', payer.publicKey.toBase58());
-    console.log('Mint PublicKey:', mint.toBase58());
-    console.log('Quantity:', quantity);
-    console.log('Payment Type:', paymentType);
-
-    const payerTokenAccount = await getOrCreateAssociatedTokenAccount(connection, payer, mint, payer.publicKey, decimals);
-    console.log('Payer Token Account:', payerTokenAccount.address.toBase58());
-
-    await mintTo(
-        connection,
-        payer,
-        mint,
-        payerTokenAccount.address,
-        payer.publicKey,
-        quantity * Math.pow(10, decimals)
-    );
-
-    console.log(`Minted ${quantity} tokens to payer token account ${payerTokenAccount.address.toBase58()} under payment type ${paymentType}`);
-    return payerTokenAccount;
 }
-
-
