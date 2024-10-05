d55c1de3fe3adc12c024ab239b727c2f81ca0822
TheLazySol <tbelonog25@gmail.com>
2024-10-05 01:30:40 -0400

    custom fonts important into tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,11 +9,28 @@ const config: Config = {
   theme: {
     extend: {
       fontFamily: {
-        'custom': ['YourCustomFont', 'sans-serif'],
-        // more custom font here
+        'p-black': ['p-black', 'sans-serif'],
+        'p-blackitalic': ['p-blackitalic', 'sans-serif'],
+        'p-bold': ['p-bold', 'sans-serif'],
+        'p-bolditalic': ['p-bolditalic', 'sans-serif'],
+        'p-extrabold': ['p-extrabold', 'sans-serif'],
+        'p-extrabolditalic': ['p-extrabolditalic', 'sans-serif'],
+        'p-extralight': ['p-extralight', 'sans-serif'],
+        'p-extralightitalic': ['p-extralightitalic', 'sans-serif'],
+        'p-italic': ['p-italic', 'sans-serif'],
+        'p-light': ['p-light', 'sans-serif'],
+        'p-lightitalic': ['p-lightitalic', 'sans-serif'],
+        'p-medium': ['p-medium', 'sans-serif'],
+        'p-mediumitalic': ['p-mediumitalic', 'sans-serif'],
+        'p-regular': ['p-regular', 'sans-serif'],
+        'p-semibold': ['p-semibold', 'sans-serif'],
+        'p-semibolditalic': ['p-semibolditalic', 'sans-serif'],
+        'p-thin': ['p-thin', 'sans-serif'],
+        'p-thinitalic': ['p-thinitalic', 'sans-serif'],
       },
     },
   },
   plugins: [],
 };
+
 export default config;
